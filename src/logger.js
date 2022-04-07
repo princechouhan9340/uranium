288265cbd0e8e6d6d9227add2e38c097c01b923d
prince chouhan <pc75805@gmail.com>
2022-04-07 12:31:14 +0530

    assignment submit

---
+++ src/logger.js
@@ -0,0 +1,8 @@
+let endpoint = "https:/www.google.com"
+
+let log = function(){
+    console.log("this is my new modules")
+}
+
+module.exports.endpoint = endpoint
+module.exports.logging = log
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
+const logger = require('../logger')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+    console.log("the end point value is", logger.endpoint)
     res.send('My first ever api!')
+    logger.logging()
 });
 
+router.get('/test-me2', function (req, res) {
+
+    res.send('My first ever api two....!')
+});
 module.exports = router;
 // adding this comment for no reason
